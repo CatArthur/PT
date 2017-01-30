37b94f8ebac5125860f0ae5a4617e44764016f57
CatArthur <mssizov@gmail.com>
2017-01-30 18:10:36 +0300

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/pt/MainActivity.java
@@ -0,0 +1,21 @@
+package com.bignerdranch.android.pt;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageButton[] i;
+    }
+}
